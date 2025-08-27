1c4c1cca359de505591f95ee933a2dfb998aab2a
Akshit Kapoor <98168037+KapoorAkshit18@users.noreply.github.com>
2025-08-27 22:00:27 +0530

    Update sum_1_to_n.c

--- Task 1/sum_1_to_n.c
+++ Task 1/sum_1_to_n.c
@@ -1,13 +1,13 @@
-#include<stdio.h>                       // standard input output header which have printf scanf etc 
-int main() {                            // function named main with a return type integer
-  
-	int i, sum=0, n=100;                     // initializing the variable for count, and addition and the range n
+#include<stdio.h>                            // standard input output header which have printf scanf etc 
 
-	for (i=1; i<=n; ++i) {                    // loop till sum of n nos. is achieved
+int main() {
+	 int i, sum=0, n=-2147483647;			 // initializing the variable for count, and addition and the range n
 
-	sum+=i;                                    // compound assignment or single step shortcut
+	for (i=1; i<=n; ++i) {					  // loop till sum of n nos. is achieved
+
+	sum+=i;										// compound assignment or single step shortcut
 }
-	printf("Sum of numbers from 1 to %d is %d\n",n, sum);           
-  
-	return 0;                                    // program is finished OS
+	printf("Sum of numbers from 1 to %d is %d\n",n, sum);
+	
+	return 0;										// program is finished OS
 }
